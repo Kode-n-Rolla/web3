6b5df03540068391dee927677b1eeabf2ea7f2af
Kode-n-Rolla <111082113+Kode-n-Rolla@users.noreply.github.com>
2025-08-16 14:13:57 -0300

    Create ILessonNine.sol

---
+++ own_kodes/nft_challange/src/ILessonNine.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/// @title ILessonNine - Interface for LessonNine challenge
+/// @notice This interface allows solving the LessonNine challenge by submitting a guess and a Twitter handle
+interface ILessonNine {
+    /// @notice Submit your solution to the challenge
+    /// @param randomGuess The guessed number to solve the challenge
+    /// @param yourTwitterHandle Your Twitter handle to be displayed on the leaderboard
+    function solveChallenge(uint256 randomGuess, string calldata yourTwitterHandle) external;
+}
